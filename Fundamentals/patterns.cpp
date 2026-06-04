d78c4ffce29e4a97aacb68991044a9e1a4ca66f9
Harshvardhan770 <harshvardhandhere19@gmail.com>
2026-06-04 19:43:25 +0530

    Update .gitignore to include tempCodeRunnerFile.cpp and enhance patterns.cpp with additional pattern generation logic

--- Fundamentals/patterns.cpp
+++ Fundamentals/patterns.cpp
@@ -176,9 +176,120 @@ int main()
         {
             cout << " ";
         }
-        for (int k = 0; k < n - i; k++)
+        for (int j = 0; j < n - i; j++)
         {
             cout << i + 1;
+        }
+        cout << endl;
+    }
+
+    cout << endl
+         << endl;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n - i - 1; j++)
+        {
+            cout << " ";
+        }
+        for (int j = 1; j <= i + 1; j++)
+        {
+            cout << j;
+        }
+        for (int j = i; j > 0; j--)
+        {
+            cout << j;
+        }
+        cout << endl;
+    }
+
+    cout << endl
+         << endl;
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n - i - 1; j++)
+        {
+            cout << " ";
+        }
+        cout << "*";
+
+        // for (int j = 0; j < i + (i - 1); j++)
+        // {
+        //     cout << " ";
+        // }
+        if (i != 0)
+        {
+            for (int j = 0; j < 2 * i - 1; j++)
+            {
+                cout << " ";
+            }
+            cout << "*";
+        }
+        cout << endl;
+    }
+
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (int j = 0; j < i + 1; j++)
+        {
+            cout << " ";
+        }
+        cout << "*";
+        // cout << endl;
+
+        if (i != n - 2)
+        {
+            for (int j = 0; j < 2 * (n - i) - 5; j++)
+            {
+                cout << " ";
+            }
+            cout << "*";
+        }
+        cout << endl;
+    }
+
+    cout << endl
+         << endl;
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < i + 1; j++)
+        {
+            cout << "*";
+        }
+        for (int j = n; j > i + 1; j--)
+        {
+            cout << " ";
+        }
+        for (int j = n; j > i + 1; j--)
+        {
+            cout << " ";
+        }
+        for (int j = 0; j < i + 1; j++)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = n; j > i; j--)
+        {
+            cout << "*";
+        }
+        for (int j = 0; j < i; j++)
+        {
+            cout << " ";
+        }
+
+        for (int j = 0; j < i; j++)
+        {
+            cout << " ";
+        }
+        for (int j = n; j > i; j--)
+        {
+            cout << "*";
         }
         cout << endl;
     }
